5070d01331eb142c139177657372757e09e06981
Danil <sse1r@mail.ru>
2025-10-14 21:31:43 +0600

    Create login register rokenResponse dtos

---
+++ AuthService/Models/DTOs/TokenResponseDto.cs
@@ -0,0 +1,9 @@
+namespace AuthService.Models.DTOs
+{
+    public class TokenResponseDto
+    {
+        public string AccessToken { get; set; } = null!;
+        public string RefreshToken { get; set; } = null!;
+        public DateTime ExpiresAt { get; set; }
+    }
+}
