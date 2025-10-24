8f137b042ef3c7b3429890b1e73e652cbb967f8a
Danil <sse1r@mail.ru>
2025-10-24 15:03:01 +0600

    Add UserController

---
+++ AuthService/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+using AuthService.Models.DTOs.Filters;
+using AuthService.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions;
+
+namespace AuthService.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(IUserService userService) 
+{
+
+    private readonly IUserService _userService = userService;
+
+    [HttpPost("search")]
+    public async Task<IActionResult> SearchUser([FromBody] SearchUserFilterDto dto)
+    {
+        var result = await _userService.SearchUser(dto);
+
+        return result.ToActionResult();
+    }
+}
